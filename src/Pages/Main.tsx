37311a4a33f4a50a659195a55da74bdd20b1d8bb
Gabriel Spalato Marques <3950465+gspalato@users.noreply.github.com>
2021-02-11 17:36:09 -0300

    Multiple changes

--- src/Pages/Main.tsx
+++ src/Pages/Main.tsx
@@ -1,13 +1,39 @@
 import React from 'react';
+import Pageable from 'pageable';
+
 import { Tile } from "../Components/Tile";
+
+type PageProps = {
+    
+}
 
 export class MainPage extends React.Component
 {
+    constructor(props: PageProps)
+    {
+        super(props);
+    }
+
+    public componentDidMount()
+    {
+        new Pageable("#container", {
+            pips: true,
+
+            onInit: function(data: PageableData)
+            {
+                
+            }
+        });
+    }
+
     public render()
     {
         return (
             <div id="container">
-                <Tile Anchor="Introduction">
+                <Tile Name="Título">
+
+                </Tile>
+                <Tile Name="Introducción">
                     
                 </Tile>
             </div>
