65d05d940cbaed6649f3bc58b1d63fc063465fa3
Gabriel Spalato Marques <3950465+gspalato@users.noreply.github.com>
2021-02-14 20:21:27 -0300

    Rewrote the main app component.

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { MainPage } from './Pages/Main';
 import './App.css';
 
-function App() {
-  return (
-    <div className="app">
-      <MainPage />
-    </div>
-  );
+export default class App extends React.Component
+{
+    public render()
+    {
+        return (
+        	<div className="app">
+        		<MainPage />
+        	</div>
+        )
+    };
 }
-
-export default App;
