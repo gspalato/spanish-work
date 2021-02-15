6e2a7e8de2bd283c55c8c4f9aeaffaf5f4174875
Gabriel Spalato Marques <3950465+gspalato@users.noreply.github.com>
2021-02-15 00:13:25 -0300

    Minor changes

--- src/Pages/Main.tsx
+++ src/Pages/Main.tsx
@@ -17,7 +17,7 @@ var ProfileImage = styled.img`
     left: 40px;
     top: 40px;
 
-    animation: gs-slide-right .75s;
+    animation: gs-slide-right 1.5s;
     animation-delay: .5s;
     animation-timing-function: ease-in-out;
 `;
@@ -36,6 +36,37 @@ var BouncingScrollArrow = styled.img`
     animation: gs-bounce 3s infinite;
 `;
 
+var IntroductionTitleContainer = styled.div`
+
+`;
+
+var IntroductionFirstTitle = styled.h1`
+    display: block;
+    position: absolute;
+
+    font-family: 'Montserrat', sans-serif;
+    font-weight: bold;
+    font-size: 14em;
+    color: rgba(255,255,255,.075);
+
+    background: linear-gradient(135deg, rgba(255,255,255,1) 0%, rgba(119,119,119,1) 50%, rgba(17,17,17,1) 100%);
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+
+    margin: 22vh 0 0 40vh;
+`;
+var IntroductionSecondTitle = styled.h1`
+    display: block;
+    position: absolute;
+
+    font-family: 'Montserrat', sans-serif;
+    font-weight: bold;
+    font-size: 12em;
+    color: #eeeeee;
+
+    margin-top: 38vh;
+`;
+
 // Main component
 export class MainPage extends React.Component
 {
@@ -49,12 +80,12 @@ export class MainPage extends React.Component
         return (
             <TileContainer>
                 <Tile Landing HideOverflowX Name="Título">
-                    <ScrollAnimate Animation="slide-right" Offset={-200}>
-                        <ProfileImage src="img/gslogo.svg"/>
-                    </ScrollAnimate>
+                    <ProfileImage src="img/gslogo.svg"/>
+                    <IntroductionFirstTitle>los</IntroductionFirstTitle>
+                    <IntroductionSecondTitle className="gs-abs-hcenter">comparativos</IntroductionSecondTitle>
                     <BouncingScrollArrow
                     src="img/thin_arrow_white.png"
-                    className="gs-center-horizontal-absolute"></BouncingScrollArrow>
+                    className="gs-abs-hcenter"></BouncingScrollArrow>
                 </Tile>
                 <Tile Name="Introducción" Background="#ffffff">
                     <ScrollAnimate Animation="fade-up">
