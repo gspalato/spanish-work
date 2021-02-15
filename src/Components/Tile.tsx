ecd49724a8c0288dc9da30bf49c781eb87b45e01
Gabriel Spalato Marques <3950465+gspalato@users.noreply.github.com>
2021-02-14 23:52:19 -0300

    Mastered styled props. Added more custom properties to tiles.

--- src/Components/Tile.tsx
+++ src/Components/Tile.tsx
@@ -1,13 +1,30 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const StyledTile = styled.div`
-    height: 100vh;
-    width: 100vw;
+/* Styled components */
+interface StyledTileProps {
+    Height: string,
+    BackgroundColor: string,
+    OverflowX: string,
+    OverflowY: string,
+};
+
+var StyledTile = styled.div<StyledTileProps>`
+    height: ${props => props.Height};
+    width: inherit;
+    background-color: ${props => props.BackgroundColor};
+
+    overflow-x: ${props => props.OverflowX};
+    overflow-y: ${props => props.OverflowY};
 `;
 
+/* Main component */
 type TileProps = {
     Name: string,
+    Landing?: boolean,
+    Background?: string,
+    HideOverflowX?: boolean,
+    HideOverflowY?: boolean,
 }
 
 export class Tile extends React.Component<TileProps>
@@ -17,14 +34,17 @@ export class Tile extends React.Component<TileProps>
     constructor(props: TileProps)
     {
         super(props);
-
         this.Name = this.props.Name;
     }
 
     public render()
     {
         return (
-            <StyledTile className="gs-tile" data-anchor={this.props.Name}>
+            <StyledTile className="gs-tile" data-anchor={this.props.Name} id={this.props.Name.toLowerCase()}
+            BackgroundColor={this.props.Background || "transparent"}
+            Height={this.props.Landing ? "100vh" : "95vh"}
+            OverflowX={this.props.HideOverflowX ? "hidden" : "unset"}
+            OverflowY={this.props.HideOverflowY ? "hidden" : "unset"}>
                 {this.props.children}
             </StyledTile>
         )
